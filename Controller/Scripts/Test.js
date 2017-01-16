c2c042a35f79b509b7c1af0e7edb1243a9b8c6ab
Herman Gouw <hermangouw@gmail.com>
2017-01-16 17:06:11 +1100

    Added more Angular JS scripts

--- Controller/Scripts/Test.js
+++ Controller/Scripts/Test.js
@@ -1,17 +1,30 @@
 ﻿/// <reference path="angular.min.js" />
 
-var myApp = angular.module("myModule", []);
+// Method chaining mechanism
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope) {
+                    $scope.message = "AngularJS Tutorial";
+                    var employee = {
+                        firstName: 'Herman',
+                        lastName: 'Gouw',
+                        gender: 'Male'
+                    };
+                    $scope.employee = employee;
+                });
 
-myApp.controller("myController", function ($scope) {
-    $scope.message = "AngularJS Tutorial";
+//var myApp = angular.module("myModule", []);
 
-    var employee = {
-        firstName: 'Herman',
-        lastName: 'Gouw',
-        gender: 'Male'
-    };
-    $scope.employee = employee;
-});
+// Combining controller assignment and creation
+//myApp.controller("myController", function ($scope) {
+//    $scope.message = "AngularJS Tutorial";
+//    var employee = {
+//        firstName: 'Herman',
+//        lastName: 'Gouw',
+//        gender: 'Male'
+//    };
+//    $scope.employee = employee;
+//});
 
 // The long form:
 //var myController = function ($scope) {
