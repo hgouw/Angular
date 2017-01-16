b35982cafb224798c028e490c68a7a56a885b4bd
Herman Gouw <hermangouw@gmail.com>
2017-01-16 15:38:54 +1100

    Added Controller project

---
+++ Controller/Scripts/Test.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+    $scope.message = "AngularJS Tutorial";
+
+    var employee = {
+        firstName: 'Herman',
+        lastName: 'Gouw',
+        gender: 'Male'
+    };
+    $scope.employee = employee;
+});
+
+// The long form:
+//var myController = function ($scope) {
+//    $scope.message = "AngularJS Tutorial";
+//};
+//myApp.controller("myController", myController);
