49d3e98c3b5867df8878077796e81f7616e6d5cd
Herman Gouw <hermangouw@gmail.com>
2017-01-16 18:22:42 +1100

    Added Repeat project

---
+++ Repeat/Scripts/Test.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope) {
+                    var members = [
+                        { firstName: "Herman", lastName: "Gouw", age: 56 },
+                        { firstName: "Helen", lastName: "Gouw", age: 45 },
+                        { firstName: "Sarah", lastName: "Gouw", age: 12 },
+                        { firstName: "Olivia", lastName: "Gouw", age: 8 }
+                    ];
+                    $scope.members = members;
+
+                    var families = [
+                         {
+                            parents: "Herman & Helen",
+                            children: [
+                                { firstName: "Sarah" },
+                                { firstName: "Olivia" }
+                            ]
+                        },
+                        {
+                            parents: "Hilly & Eunyoung",
+                            children: [
+                                { firstName: "Rebecca" }
+                            ]
+                        },
+                    ];
+                    $scope.families = families;
+                });
