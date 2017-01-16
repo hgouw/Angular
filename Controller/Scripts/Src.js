2e95d59338078f6f32f24558d6743d0384c676f7
Herman Gouw <hermangouw@gmail.com>
2017-01-16 17:07:08 +1100

    Added ng-src sample

---
+++ Controller/Scripts/Src.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="angular.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope) {
+                    $scope.message = "AngularJS Tutorial";
+                    var country = {
+                        name: 'Australia',
+                        capital: 'Canberra',
+                        flag: '/Images/aus1-flag.jpg'
+                    };
+                    $scope.country = country;
+                });
