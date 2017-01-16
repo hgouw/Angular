5318f6c1fb7632299e5ca267e0b641534c86d584
Herman Gouw <hermangouw@gmail.com>
2017-01-16 17:23:30 +1100

    Added DataBinding project

---
+++ DataBinding/Scripts/Test.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="angular.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope) {
+                    $scope.message = "Hello AngularJS!";
+                });
