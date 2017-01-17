9e858e24526386516f04a3bce243425729a3bbda
Herman Gouw <hermangouw@gmail.com>
2017-01-17 11:33:06 +1100

    Added Events project

---
+++ Events/Scripts/Test.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope) {
+                    var clouds = [
+                        { name: "Azure", likes: 0, dislikes: 0 },
+                        { name: "AWS", likes: 0, dislikes: 0 },
+                        { name: "Google", likes: 0, dislikes: 0 }
+                    ];
+
+                    $scope.clouds = clouds;
+
+                    $scope.incrementLikes = function (cloud) {
+                        cloud.likes++;
+                    }
+
+                    $scope.incrementDislikes = function (cloud) {
+                        cloud.dislikes++;
+                    }
+                });
