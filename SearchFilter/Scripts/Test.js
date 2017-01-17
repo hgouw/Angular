8e872dfb3cca7171aa08ac049f76de43728d8084
Herman Gouw <hermangouw@gmail.com>
2017-01-17 17:09:24 +1100

    Added a custom filter

--- SearchFilter/Scripts/Test.js
+++ SearchFilter/Scripts/Test.js
@@ -4,9 +4,9 @@ var myApp = angular
                 .module("myModule", [])
                 .controller("myController", function ($scope) {
                     var employees = [
-                        { name: "Peter", gender: "Male", salary: 110000.00, suburb: "Balgowlah" },
-                        { name: "Scott", gender: "Male", salary: 120000.00, suburb: "Middle Dural" },
-                        { name: "Tom", gender: "Male", salary: 140000.00, suburb: "Ashfield" }
+                        { name: "Peter", gender: 1, salary: 110000.00, suburb: "Balgowlah" },
+                        { name: "Scott", gender: 3, salary: 120000.00, suburb: "Middle Dural" },
+                        { name: "Tom", gender: 1, salary: 140000.00, suburb: "Ashfield" }
                     ];
 
                     $scope.employees = employees;
