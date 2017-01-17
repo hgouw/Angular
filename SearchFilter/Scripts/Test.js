5ebcd43b9540eb209b37d2a6028f40684b406e67
Herman Gouw <hermangouw@gmail.com>
2017-01-17 16:13:01 +1100

    Implemented Filter by Multiple Properties

--- SearchFilter/Scripts/Test.js
+++ SearchFilter/Scripts/Test.js
@@ -4,9 +4,9 @@ var myApp = angular
                 .module("myModule", [])
                 .controller("myController", function ($scope) {
                     var employees = [
-                        { name: "Peter", gender: "Male", salary: 110000.00 },
-                        { name: "Scott", gender: "Male", salary: 120000.00 },
-                        { name: "Tom", gender: "Male", salary: 140000.00 }
+                        { name: "Peter", gender: "Male", salary: 110000.00, suburb: "Balgowlah" },
+                        { name: "Scott", gender: "Male", salary: 120000.00, suburb: "Middle Dural" },
+                        { name: "Tom", gender: "Male", salary: 140000.00, suburb: "Ashfield" }
                     ];
 
                     $scope.employees = employees;
