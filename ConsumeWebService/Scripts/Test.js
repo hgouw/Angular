c065d50afc6917a136f4e2a152434e884f344395
Herman Gouw <hermangouw@gmail.com>
2017-01-18 15:29:33 +1100

    Added $log

--- ConsumeWebService/Scripts/Test.js
+++ ConsumeWebService/Scripts/Test.js
@@ -2,10 +2,12 @@
 
 var myApp = angular
                 .module("myModule", [])
-                .controller("myController", function ($scope, $http) {
+                .controller("myController", function ($scope, $http, $log) {
+                    //shortcut method
                     //$http.get('CompanyService.asmx/GetAllCompanies')
                     $http({method: 'GET', url: 'CompanyService.asmx/GetAllCompanies'})                       
                          .then(function (response) {
                              $scope.companies = response.data;
+                             $log.info(response); //useful for debugging
                          });
                 });
