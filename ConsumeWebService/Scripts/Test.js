5d93917abc6a1bd5590f32a91cfe658e7434b97d
Herman Gouw <hermangouw@gmail.com>
2017-01-18 15:15:23 +1100

    Added the alternative way of calling $http.get

--- ConsumeWebService/Scripts/Test.js
+++ ConsumeWebService/Scripts/Test.js
@@ -3,7 +3,8 @@
 var myApp = angular
                 .module("myModule", [])
                 .controller("myController", function ($scope, $http) {
-                    $http.get('CompanyService.asmx/GetAllCompanies')
+                    //$http.get('CompanyService.asmx/GetAllCompanies')
+                    $http({method: 'GET', url: 'CompanyService.asmx/GetAllCompanies'})                       
                          .then(function (response) {
                              $scope.companies = response.data;
                          });
