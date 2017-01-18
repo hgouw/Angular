b09ed93886b420444e028f0bdae4e11620ac0605
Herman Gouw <hermangouw@gmail.com>
2017-01-18 18:22:22 +1100

    Added Routing project

---
+++ Routing/Scripts/Test.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="angular.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope) {
+                });
