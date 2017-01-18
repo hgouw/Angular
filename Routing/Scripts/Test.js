e23ae7fd6bb2047a619e6ae0dfd3122102415fc9
Herman Gouw <hermangouw@gmail.com>
2017-01-19 10:26:47 +1100

    Implemented Partial Templates

--- Routing/Scripts/Test.js
+++ Routing/Scripts/Test.js
@@ -1,6 +1,27 @@
 ﻿/// <reference path="angular.js" />
 
-var myApp = angular
-                .module("myModule", [])
-                .controller("myController", function ($scope) {
-                });
+var app = angular.module("myModule", ["ngRoute"])
+                 .config(function ($routeProvider) {
+                     $routeProvider
+                        .when("/home", {
+                            templateUrl: "Templates/Home.html",
+                            controller: "homeController"
+                        })
+                        .when("/courses", {
+                            templateUrl: "Templates/Courses.html",
+                            controller: "coursesController"
+                        })
+                        .when("/students", {
+                            templateUrl: "Templates/Students.html",
+                            controller: "studentsController"
+                        })
+                 })
+                 .controller("homeController", function ($scope) {
+                     $scope.message = "Home Page";
+                 })
+                 .controller("coursesController", function ($scope) {
+                     $scope.courses = ["C#", ".NET", "LINQ", "Entity Framework", "SQL Server", "Azure"];;
+                 })
+                 .controller("studentsController", function ($scope) {
+                     $scope.students = ["Fergus", "Goran", "Herman", "Michael"];
+                 });
