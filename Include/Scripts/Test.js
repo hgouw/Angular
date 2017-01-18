b4a8cfd9cdfdc467bdd292fe7b24821e436cfc77
Herman Gouw <hermangouw@gmail.com>
2017-01-18 14:14:17 +1100

    Added Include project

---
+++ Include/Scripts/Test.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope) {
+                    var employees = [
+                        { name: "Peter", gender: "Male", salary: 110000.00 },
+                        { name: "Scott", gender: "Male", salary: 120000.00 },
+                        { name: "Tom",   gender: "Male", salary: 140000.00 }
+                    ];
+                    $scope.employees = employees;
+                    $scope.employeeView = "EmployeeTable.html";
+                });
