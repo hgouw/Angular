a174c620c8598f7064ac156574c572ffb5c88bb0
Herman Gouw <hermangouw@gmail.com>
2017-01-18 15:06:05 +1100

    Added ConsumeWebService project

---
+++ ConsumeWebService/CompanyService.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+
+namespace ConsumeWebService
+{
+    /// <summary>
+    /// Summary description for CompanyService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class CompanyService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public void GetAllCompanies()
+        {
+            List<Company> companies = new List<Company>();
+            var cs = ConfigurationManager.ConnectionStrings["ASX"].ConnectionString;
+            using (var connection = new SqlConnection(cs))
+            {
+                var command = new SqlCommand("select top 10 * from Companies", connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var company = new Company
+                    {
+                        Code = reader["Code"].ToString(),
+                        Name = reader["Name"].ToString(),
+                        Group = reader["Group"].ToString()
+                    };
+                    companies.Add(company);
+                }
+            }
+            var js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(companies));
+        }
+    }
+}
---
+++ ConsumeWebService/Scripts/Test.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="angular.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope, $http) {
+                    $http.get('CompanyService.asmx/GetAllCompanies')
+                         .then(function (response) {
+                             $scope.companies = response.data;
+                         });
+                });
