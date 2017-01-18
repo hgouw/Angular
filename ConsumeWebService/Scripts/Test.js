8e65449ed9004e1fea7045dd405c920f2c58211b
Herman Gouw <hermangouw@gmail.com>
2017-01-18 16:29:45 +1100

    Added separate Success and Error callback functions
    Converted the anonymous functions into separate success and error
    callback functions

--- ConsumeWebService/Scripts/Test.js
+++ ConsumeWebService/Scripts/Test.js
@@ -5,12 +5,24 @@ var myApp = angular
                 .controller("myController", function ($scope, $http, $log) {
                     //shortcut method
                     //$http.get('CompanyService.asmx/GetAllCompanies')
-                    $http({method: 'GET', url: 'CompanyService.asmx/GetAllCompanies'})                       
-                         .then(function (response) {
-                             $scope.companies = response.data;
-                             $log.info(response); //useful for debugging
-                         }, function (reason) {
-                             $scope.error = reason.data;
-                             $log.info(reason);
-                         });
+
+                    var successCallBack = function (response) {
+                        $scope.companies = response.data;
+                        $log.info(response);
+                    };
+
+                    var errorCallBack = function (reason) {
+                        $scope.error = reason.data;
+                        $log.info(reason);
+                    }
+
+                    $http({ method: 'GET', url: 'CompanyService.asmx/GetAllCompanies' })
+                         //.then(function (response) {
+                         //    $scope.companies = response.data;
+                         //    $log.info(response); //useful for debugging
+                         //}, function (reason) {
+                         //    $scope.error = reason.data;
+                         //    $log.info(reason);
+                         //});
+                         .then(successCallBack, errorCallBack);
                 });
