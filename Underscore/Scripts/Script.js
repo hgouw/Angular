14a6ab85ffd0f706ca8d5efc603d9cbb804fcce4
Herman Gouw <hermangouw@gmail.com>
2017-02-08 09:50:39 +1100

    Added Underscore project

---
+++ Underscore/Scripts/Script.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.js" />
+
+_.each([1, 2, 3], alert);
+=> alerts each number in turn...
+_.each({one: 1, two: 2, three: 3}, alert);
+=> alerts each number value in turn...
+
+var evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+=> [2, 4, 6]
+
+var odds = _.reject([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+=> [1, 3, 5]
