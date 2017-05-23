9a4ba17712bddf60094685191233765516a70459
Herman Gouw <hermangouw@gmail.com>
2017-05-23 15:28:43 +1000

    Added Authorisation using the cookie value

--- Cookie/Scripts/Script.js
+++ Cookie/Scripts/Script.js
@@ -2,8 +2,19 @@
 /// <reference path="angular-cookies.js" />
 
 var myApp = angular
-                .module("myModule", [])
-                .controller("myController", function ($http) {
+                .module("myModule", ["ngCookies"])
+                .controller("myController", function ($http, $cookies) {
+                    var cookie = $cookies.get('lna2');
+                    var token;
+                    var url;
+
+                    if (typeof cookie !== 'undefined') {
+                        token = "hca|" + cookie;
+                        url = "https://ecc9oe0bo0.execute-api.ap-southeast-2.amazonaws.com/dev/ca-auth?limit=1&num=1&sort=daten";
+                    }
+                    else {
+                        url = "https://ecc9oe0bo0.execute-api.ap-southeast-2.amazonaws.com/dev/ca-rds?limit=2&num=2&sort=daten";
+                    }
 
                     var successCallBack = function (response) {
                         document.getElementById("data").innerHTML = JSON.stringify(response.data, undefined, 2);
@@ -15,7 +26,10 @@ var myApp = angular
 
                     $http({
                         method: "GET",
-                        url: "https://ecc9oe0bo0.execute-api.ap-southeast-2.amazonaws.com/dev/ca-rds?limit=1&num=1&sort=daten"
+                        headers: {
+                            'Authorization': token
+                        },
+                        url: url
                     })
                     .then(successCallBack, errorCallBack);
                 });
