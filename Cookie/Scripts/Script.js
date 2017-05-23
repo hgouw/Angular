760494cf475b46b2775051ecce201111b3c4f381
Herman Gouw <hermangouw@gmail.com>
2017-05-23 11:31:23 +1000

    Added Cookie project

---
+++ Cookie/Scripts/Script.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="angular.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope, $http, $log) {
+
+                    var successCallBack = function (response) {
+                        document.getElementById("json").innerHTML = JSON.stringify(response.data, undefined, 2);
+                    };
+
+                    var errorCallBack = function (reason) {
+                        $scope.error = reason.data;
+                        console.log(reason);
+                    }
+
+                    $http({
+                        method: "GET",
+                        url: "https://ecc9oe0bo0.execute-api.ap-southeast-2.amazonaws.com/dev/ca-rds?limit=1&num=1&sort=daten"
+                    })
+                    .then(successCallBack, errorCallBack);
+                });
