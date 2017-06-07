eb7eb7d544ac5b19524f1bdd828c9124eeaeefed
Herman Gouw <hermangouw@gmail.com>
2017-06-07 13:50:04 +1000

    Added angular.forEach

--- ForEach/Scripts/Script.js
+++ ForEach/Scripts/Script.js
@@ -3,14 +3,26 @@
 var myApp = angular
                 .module("myModule", [])
                 .controller("myController", function ($scope) {
-                    var myArray = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
-                    for (var index in myArray) {
+                    var myArray = [];
+                    for (i = 0; i < 20; i++) {
+                        var myObj = { value: i+1 };
+                        myArray.push(myObj);
+                    }
+
+                    angular.forEach(myArray, function (obj, index) {
                         if (index < 12) {
-                            console.log(myArray[index]);
+                            console.log(index + " - " + obj.value);
                         }
-                        else {
-                            console.log('break');
-                            break;
-                        }
-                    };
+                    });
+
+                    //var myArray = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+                    //for (var index in myArray) {
+                    //    if (index < 12) {
+                    //        console.log(myArray[index]);
+                    //    }
+                    //    else {
+                    //        console.log('break');
+                    //        break;
+                    //    }
+                    //};
                 });
