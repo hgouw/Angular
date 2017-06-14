2117395d3664640a829214dcc0f4ab446ea4aacd
Herman Gouw <hermangouw@gmail.com>
2017-06-14 17:04:29 +1000

    Added Script2.js

---
+++ Cookie/Scripts/Script2.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="angular.js" />
+/// <reference path="angular-cookies.js" />
+
+var myApp = angular
+                .module("myModule", ["ngCookies"])
+                .controller("myController", function ($http, $cookies) {
+                    var url = "https://devapi-caseanalyser.lexisnexis.com/dev/cookie-reader";
+
+                    var successCallBack = function (response) {
+                        document.getElementById("url").innerHTML = url;
+                        document.getElementById("cookie").innerHTML = response.data.auth;
+                    };
+
+                    var errorCallBack = function (reason) {
+                        document.getElementById("url").innerHTML = url;
+                        document.getElementById("error").innerHTML = JSON.stringify(reason.data, undefined, 2);
+                    }
+
+                    $http({
+                        method: "GET",
+                        withCredentials: true,
+                        url: url
+                    })
+                    //.then(function (response) { cookie = response.data.auth; });
+                    .then(successCallBack, errorCallBack);
+                });
