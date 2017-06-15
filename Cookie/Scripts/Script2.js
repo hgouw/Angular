77da5fee17b3b9bb8da9ad462ec2523583d75aaf
Herman Gouw <hermangouw@gmail.com>
2017-06-15 11:11:43 +1000

    Refactored

--- Cookie/Scripts/Script2.js
+++ Cookie/Scripts/Script2.js
@@ -4,16 +4,16 @@
 var myApp = angular
                 .module("myModule", ["ngCookies"])
                 .controller("myController", function ($http, $cookies) {
-                    var url = "https://ecc9oe0bo0.execute-api.ap-southeast-2.amazonaws.com/dev/cookie-reader";
+                    var url = "https://devapi-caseanalyser.lexisnexis.com/dev/cookie-reader";
 
                     var successCallBack = function (response) {
                         document.getElementById("url").innerHTML = url;
-                        document.getElementById("cookie").innerHTML = response.data.auth;
+                        document.getElementById("data").innerHTML = response.data.auth;
                     };
 
                     var errorCallBack = function (reason) {
                         document.getElementById("url").innerHTML = url;
-                        document.getElementById("error").innerHTML = JSON.stringify(reason.data, undefined, 2);
+                        document.getElementById("data").innerHTML = JSON.stringify(reason.data, undefined, 2);
                     }
 
                     $http({
@@ -21,6 +21,5 @@ var myApp = angular
                         withCredentials: true,
                         url: url
                     })
-                    //.then(function (response) { cookie = response.data.auth; });
                     .then(successCallBack, errorCallBack);
                 });
