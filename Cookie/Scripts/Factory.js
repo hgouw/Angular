c236f4cdb76c1d50ebfb4c8aace74e437bd365f4
Herman Gouw <hermangouw@gmail.com>
2017-06-26 17:05:01 +1000

    Added Factory.js

---
+++ Cookie/Scripts/Factory.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="Test.js" />
+
+myApp.factory("Factory", function ($http, $q) {
+    return {
+        cookieReader: function () {
+            var deferred = $q.defer();
+
+            function callApi() {
+                $http({
+                    method: "GET",
+                    url: "https://ecc9oe0bo0.execute-api.ap-southeast-2.amazonaws.com/dev/ca-rds?limit=1&num=10&sort=daten"
+                })
+                .success(function (data, status, headers, config) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status, headers, config) {
+                    deferred.reject("apiSearch: " + status);
+                })
+            };
+
+            callApi();
+
+            return deferred.promise;
+        }
+    };
+});
