9bb24beffdbd3ccaf025c945d3b06c592119f19e
Herman Gouw <hermangouw@gmail.com>
2017-06-26 16:14:20 +1000

    Added Service.js

---
+++ Cookie/Scripts/Service.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="Test.js" />
+
+myApp.service("AuthService", function ($http, $q) {
+    var deferred = $q.defer();
+
+    this.cookieReader = function () {
+        return $http({
+            method: "GET",
+            withCredentials: true,
+            url: "https://devapi-caseanalyser.lexisnexis.com/dev/cookie-reader2"
+        })
+        .then(function (response) {
+            deferred.resolve(response.data);
+            return deferred.promise;
+        }, function (response) {
+            deferred.reject(response);
+            return deferred.promise;
+        });
+    };
+});
--- Cookie/Scripts/Test.js
+++ Cookie/Scripts/Test.js
@@ -2,25 +2,6 @@
 
 var myApp = angular.module("myModule", []);
 
-myApp.service("AuthService", function ($http, $q) {
-    var deferred = $q.defer();
-
-    this.cookieReader = function () {
-        return $http({
-            method: "GET",
-            withCredentials: true,
-            url: "https://devapi-caseanalyser.lexisnexis.com/dev/cookie-reader2"
-        })
-        .then(function (response) {
-            deferred.resolve(response.data);
-            return deferred.promise;
-        }, function (response) {
-            deferred.reject(response);
-            return deferred.promise;
-        });
-    };
-});
-
 myApp.controller("myController", function ($scope, $q, AuthService) {
     AuthService.cookieReader()
     .then(
