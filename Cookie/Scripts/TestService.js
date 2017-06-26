0dae8dcdd81d321f9e56b8b19fff19d31eddddfe
Herman Gouw <hermangouw@gmail.com>
2017-06-27 09:50:38 +1000

    Added TestService.js and TestService.html

---
+++ Cookie/Scripts/TestService.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.js" />
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope, $q, Service) {
+    Service.cookieReader()
+    .then(
+        function (result) {
+            $scope.serviceAuth = result.auth;
+        },
+        function (error) {
+            $scope.serviceAuth = error.statusText;
+        }
+    );
+});
