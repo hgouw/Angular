78dff733df496e9a4e335633ba36b59ec6588015
Herman Gouw <hermangouw@gmail.com>
2017-08-21 14:38:25 +1000

    Added ngCookies

--- Cookie/Scripts/TestFactory.js
+++ Cookie/Scripts/TestFactory.js
@@ -1,15 +1,20 @@
 ﻿/// <reference path="angular.js" />
 /// <reference path="angular-cookies.js" />
 
-var myApp = angular.module("myModule", []);
+var myApp = angular.module("myModule", ["ngCookies"]);
 
-myApp.controller("myController", function ($scope, Factory) {
+myApp.controller("myController", function ($cookies, $scope, Factory) {
     Factory.cookieReader()
     .then(function (response) {
         $scope.factoryAuth = response.data.auth;
+        $cookies.put('auth', $scope.factoryAuth);
         Factory.apiSearch($scope.factoryAuth)
         .then(function (response) {
-            $scope.factoryApi = JSON.stringify(response.data, undefined, 2);
+            $scope.factoryApiSearch = JSON.stringify(response.data, undefined, 2);
         });
+        if ($cookies.get('auth') != "")
+            $scope.cookie = "The cookie is " + $cookies.get('auth');
+        else
+            $scope.cookie = "There is no cookie";
     });
 });
