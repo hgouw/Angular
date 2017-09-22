17f090e37bade5a8126a23e9e856f317c9b848a6
Herman Gouw <hermangouw@gmail.com>
2017-09-22 14:45:22 +1000

    Refactored

--- Check/Scripts/Script.js
+++ Check/Scripts/Script.js
@@ -4,12 +4,9 @@ var myApp = angular
                 .module("myModule", [])
                 .controller("myController", function ($scope) {
                     $scope.init = function () {
-                        alert("init");
                         $scope.flag = true;
                     };
                     $scope.toggle = function () {
-                        alert("toggle");
                         $scope.flag = !filter;
                     };
                 });
-
