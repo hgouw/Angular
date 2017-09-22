7a51bca2bcfcc7731ee50e8342fa1e1f18f6c041
Herman Gouw <hermangouw@gmail.com>
2017-09-22 14:31:05 +1000

    Added Script.js

---
+++ Check/Scripts/Script.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.js" />
+
+var myApp = angular
+                .module("myModule", [])
+                .controller("myController", function ($scope) {
+                    $scope.init = function () {
+                        alert("init");
+                        $scope.flag = true;
+                    };
+                    $scope.toggle = function () {
+                        alert("toggle");
+                        $scope.flag = !filter;
+                    };
+                });
+
